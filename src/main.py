87f556300625b7b9e222887faf70c55abd1aa5dc
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-20 17:58:34 -0300

    Doc

--- src/main.py
+++ src/main.py
@@ -7,28 +7,32 @@ from utils import isNumberOrDot, ACCEPT_KEYS, numberEquation, onlyNumer, isSpeci
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    '''MainWidow herdando do arquivo gerado pelo QtDesigner'''
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
-        self.setupUi(self)
-        self._buttons = self.findChildren(QPushButton)
+        self.setupUi(self) # Obrigatorio
+        self._buttons = self.findChildren(QPushButton)# Pegando todos os botões existentes na janela
+        # Todos os botões que são numeros ou '.' :
         self.numberButtonsList = [btn for btn in self._buttons if isNumberOrDot(btn.text())]
         self._leftNumber = 0
         self._rightNumber = 0
         self._operator = None
         self._result = None
         self._equation = None
-        self._insertKeyPressed()
-        self.connections_buttons()
+        self._insertKeyPressed() # Validação para permetir apenas numeros e operadores pelo teclado
+        self.connections_buttons() # Conetando o evento click aos botões
 
     def _insertBtnTextToDisplay(self, btn : QPushButton):
-        def inner():
-            buttonText = btn.text()
+        '''Insere no display da calculadora o numero clicado ou a função dos botçoes especiais'''
+        def inner(): # Função Interna para Adiar o Retorno da Função
+            buttonText = btn.text() # Pegando o Texto do Botão
+            # Texto já existente no display mais o do botão
             numberDisplay = self.display.text() + buttonText
-            if isSpecialKeys(buttonText):
+            if isSpecialKeys(buttonText): # Verifica se é um botão especial
                 if buttonText == 'C':
-                    self.defaultResult()
+                    self.defaultResult() # Reseta todos os campos
                 if buttonText == '=':
-                    self._rightNumber = self.display.text()
+                    self._rightNumber = self.display.text() # Pegando o Texto do display
                     equation = f'{self._leftNumber} {self._operator} {self._rightNumber}'
                     self._equation = equation
                     result = self.equationResult(equation)
@@ -36,22 +40,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     self.display.setText('')
                     return self.nextEquation(result)
                 if buttonText == '<':
-                    return self.display.backspace()
+                    return self.display.backspace() # Evento para apagar unico caractere no display
 
-            if not isNumberOrDot(buttonText):
-                self.insertResultText(numberDisplay)
-                self.display.setText('')
-            return self.display.insert(buttonText)
+            if not isNumberOrDot(buttonText): # Verifica se não é um numero ou '.'
+                self.insertResultText(numberDisplay) # Insere o caracter na Label: Result
+                self.display.setText('') # Reseta o Texto do Display
+            return self.display.insert(buttonText) # Insere o texto do botão númerico no display
         return inner
 
     def equationResult(self, equation):
-        if self._operator == '^':
+        '''Resultado da equação iformada'''
+        if self._operator == '^': # Se o caractere for ^faz a exponenciação
             equationResult = pow(float(self._leftNumber), float(self._rightNumber))
         else:
-            equationResult = eval(equation)
+            equationResult = eval(equation) # Le um texto e executa como codigo
+            # Após várias validações pode-se usar o eval
         return equationResult
 
     def insertResultText(self, string):
+        '''Insere na (Label: Result) a equação imcompleta ou completa'''
         self.setNumbersEquation(string)
         if self._rightNumber == None:
             self._rightNumber = '??'
@@ -64,11 +71,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.result.setText(f'{equation} = ')
 
     def nextEquation(self, equationResult):
+        ''' Após a primeira equação, o resultado fica salvo para continuar do mesmo número'''
         self._result = equationResult
         self._leftNumber = equationResult
         self._rightNumber = 0
 
     def defaultResult(self):
+        ''' Reseta todos os campos para uma nova equação'''
         self._result = None
         self._leftNumber = 0
         self._rightNumber = 0
@@ -77,6 +86,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.result.setText('Resultado')
 
     def connections_buttons(self):
+        '''Conectando o evento de click aos botões'''
         btn : QPushButton
         for btn in self._buttons:
             buttonSlot = self._insertBtnTextToDisplay(btn)
@@ -84,6 +94,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
     def setNumbersEquation(self, string):
+        '''Definindo os números para a equação'''
         if string == '':
             left_number = 0
             right_number = 0
@@ -103,13 +114,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
+        ''' Evento de teclado no display '''
         keyPressed = event.text()
         if keyPressed == '\r':
             self.insertResultText(self.display.text())
             self.display.setText('')
 
     def _insertKeyPressed(self):
-        validator = QRegularExpressionValidator()
+        ''' Fazendo uma validação no evento de teclado para que seja somente válido inserir numeros e operadores'''
+        validator = QRegularExpressionValidator() # Adcionando a validação em regex
         validator.setRegularExpression(ACCEPT_KEYS)
         self.display.setValidator(validator)
 
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,29 @@
 import re
 
-NUM_OR_DOT = re.compile(r'^[0-9.]$')
-SPECIAL_KEYS = r"^[<C=]"
-ACCEPT_KEYS = r"^\d*\.?\d+([+\-*/^]\d*\.?\d+)*$"
-ACCEPT_KEYS_GROUPS = r"^(\d*\.?\d+)([+\-*/^])(\d*\.?\d+)*$"
+NUM_OR_DOT = re.compile(r'^[0-9.]$') # Somente numeros de 0-9 e o "."
+SPECIAL_KEYS = r"^[<C=]" # Caracteres dos botões especiais "<" "C" "="
+ACCEPT_KEYS = r"^\d*\.?\d+([+\-*/^]\d*\.?\d+)*$" # Caracteres que serão permitidas no display
+# Caracteres exibidos no display separados em grupos = num / operator / num
+ACCEPT_KEYS_GROUPS = r"^(\d*\.?\d+)([+\-*/^])(\d*\.?\d+)*$" 
 
 
 
 def isNumberOrDot(string):
+    '''Verificar se é numero ou "."
+    return: True | False
+    '''
     return bool(NUM_OR_DOT.search(string))
 
 
 def numberEquation(string):
+    ''' Tranforma o texto em num / operator / num
+
+    return:
+        Retorna os numeros antes do operador
+        O operador
+        Os numeros depois do operador
+    
+    '''
     if string in '+-/*^':
         return None, string, None
     result = re.match(ACCEPT_KEYS_GROUPS, string)
@@ -21,10 +33,12 @@ def numberEquation(string):
     return left_number, operator, right_number
 
 def onlyNumer(string):
+    ''' Pegar somente os números de um texto'''
     result = re.findall(r'\d+\.?\d*', string)
     return result[0]
 
 def isSpecialKeys(string):
+    '''Verifica se é um caracter de um botão especial'''
     specialKeys = re.compile(SPECIAL_KEYS)
     result = re.findall(specialKeys, string)
     return bool(result)
